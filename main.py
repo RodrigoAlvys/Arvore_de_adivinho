b9d2169a652957583bd55b60ad6d524aa8c50fba
Rodrigo Alves <rodrigoalvys@gmail.com>
2026-03-23 22:30:02 -0300

    Finalização do projeto - Criado menu no main.py - Opção de jogar o Akinator - Comparação de tempo entre BFS e DFS

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from source.grafo import *
+from time import perf_counter
 
 akinator = Akinator()
 akinator.load_json("init.json")
-akinator.start()
 
+while True:
+    print("""
+      /$$$$$$  /$$       /$$                       /$$                        
+     /$$__  $$| $$      |__/                      | $$                        
+    | $$  \\ $$| $$   /$$ /$$ /$$$$$$$   /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$ 
+    | $$$$$$$$| $$  /$$/| $$| $$__  $$ |____  $$|_  $$_/   /$$__  $$ /$$__  $$
+    | $$__  $$| $$$$$$/ | $$| $$  \\ $$  /$$$$$$$  | $$    | $$  \\ $$| $$  \\__/
+    | $$  | $$| $$_  $$ | $$| $$  | $$ /$$__  $$  | $$ /$$| $$  | $$| $$      
+    | $$  | $$| $$ \\  $$| $$| $$  | $$|  $$$$$$$  |  $$$$/|  $$$$$$/| $$      
+    |__/  |__/|__/  \\__/|__/|__/  |__/ \\_______/   \\___/   \\______/ |__/      
+                                                                          
+                                                                          
+""")
+    print("Menu:\n1 - Akinator\n2 - BFS vs DFS\n3 - sair")
+    x = str(input("-> ")).strip()
+    if x == "1":
+        akinator.start()
+    elif x == "2":
+        inic = perf_counter()
+        akinator.BFS()
+        end = perf_counter()
+        temp_BFS = end-inic
+
+        inic = perf_counter()
+        akinator.DFS()
+        end = perf_counter()
+        temp_DFS = end-inic
+        print(f"BFS tempo: {temp_BFS}")
+        print(f"DFS tempo: {temp_DFS}")
+        input("...")
+    elif x == "3":
+        break
+    else:
+        print("Porfavor digite as opções no meu!")
+        input("...")
+    print("\n\n\n")
 
